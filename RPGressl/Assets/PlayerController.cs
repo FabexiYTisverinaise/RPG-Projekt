4ec0915c707430afab5bde5779637cafc4f9df45
FabexiYTisverinaise <g2turtle@yahoo.com>
2022-11-12 01:39:26 +0100

    Movement + Colissions

--- RPGressl/Assets/PlayerController.cs
+++ RPGressl/Assets/PlayerController.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Vector2 movementInput;
+    Rigidbody2D rb;
+    public float moveSpeed = 1f;
+    public ContactFilter2D movementFilter;
+    public float collisionOfsset = 0.05f;
+
+    List<RaycastHit2D> castColissions = new List<RaycastHit2D>();
+
+    
+
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void FixedUpdate()
+    {
+        if (movementInput != Vector2.zero)
+        {
+            int count = rb.Cast(
+            movementInput,
+            movementFilter,
+            castColissions,
+            moveSpeed * Time.fixedDeltaTime + collisionOfsset);
+            if (count == 0)
+            {
+                rb.MovePosition(rb.position + movementInput * moveSpeed * Time.fixedDeltaTime);
+            }
+        }
+    }
+    void OnMove(InputValue movementValue)
+    {
+        movementInput = movementValue.Get<Vector2>();
     }
 }
